852145b762f8086cb3f67174a82ea90464c3698b
castanedaLuis <castanedaLuis@github.com>
2023-01-27 23:58:50 -0600

    Creación del layout principal e integración de los Page Components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
 import '@styles/tailwind.css'
+import Layout from '@layout/MainLayout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+    </>
+  )
 }
 
 export default MyApp
