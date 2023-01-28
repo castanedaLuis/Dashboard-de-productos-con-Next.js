f0e3e18ac9e6d27b2b5eedceaa66872bc38cc24e
castanedaLuis <castanedaLuis@github.com>
2023-01-28 00:33:00 -0600

    estructura de la API

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: 'v1',
+    NEXT_PUBLIC_API_VERSION:'https://api.escuelajs.co'
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
