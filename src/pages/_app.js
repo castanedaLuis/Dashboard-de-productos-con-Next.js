6bc5012f3e40e93617dfe225ed783f671a057457
castanedaLuis <castanedaLuis@github.com>
2023-01-28 01:05:47 -0600

    Creación del custom hook useAuth con useContext

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,15 @@
 import '@styles/tailwind.css'
 import Layout from '@layout/MainLayout'
+import { ProviderAuth } from '@hooks/useAuth'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <ProviderAuth>
         <Layout>
           <Component {...pageProps} />
         </Layout>
+      </ProviderAuth>
     </>
   )
 }
