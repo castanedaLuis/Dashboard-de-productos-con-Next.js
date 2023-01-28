eaadd4e1ef7a9ea5c15511603b8ec06caede79f2
castanedaLuis <castanedaLuis@github.com>
2023-01-28 12:02:00 -0600

    Guardado del token en una cookie para mantener la sesión

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -18,31 +18,41 @@ function useProvideAuth() {
   const [user, setUser] = useState(null);
   const [error, setError]= useState(false);
 
-//   const signIn = async (email, password) => {
-//     const options = {
-//       headers: {
-//         accept: '*/*',
-//         'Content-Type': 'application/json',
-//       },
-//     };
-//     const { data: access_token } = await axios.post(endPoints.auth.login, { email, password }, options);
-//     console.log('token', access_token);
-//   };
-    const signIn = async (email, password) => {
-        const response = await fetch('https://api.escuelajs.co/api/v1/auth/login', {
-            method: 'POST',
-            headers: {
-                accept: '*/*',
-                'Content-Type': 'application/json;charset=utf-8',
-            },
-            body: JSON.stringify({ email, password }),
-        });
-        const data = await response.json();
-        //console.log(data.access_token);
-        if(data.access_token){
-            Cookie.set('token', data.access_token, { expires:5 } );
-        }
+  const signIn = async (email, password) => {
+    const options = {
+      headers: {
+        accept: '*/*',
+        'Content-Type': 'application/json',
+      },
     };
+    const { data: access_token } = await axios.post('https://api.escuelajs.co/api/v1/auth/login', { email, password }, options);
+
+    if(access_token.access_token){
+        const token = access_token.access_token
+        Cookie.set('token', token, { expires:5 } );
+
+        // Ok ya tenemis el token, lo guardamos en una cookie y hacemos un default para que se implemente en la sig peticiones.
+        axios.defaults.headers.Authorization = `Bearer ${token}`
+        const { data: user } = await axios.get('https://api.escuelajs.co/api/v1/auth/profile');
+        console.log(user);
+        setUser(user) 
+    }
+  };
+    // const signIn = async (email, password) => {
+    //     const response = await fetch('https://api.escuelajs.co/api/v1/auth/login', {
+    //         method: 'POST',
+    //         headers: {
+    //             accept: '*/*',
+    //             'Content-Type': 'application/json;charset=utf-8',
+    //         },
+    //         body: JSON.stringify({ email, password }),
+    //     });
+    //     const data = await response.json();
+    //     //console.log(data.access_token);
+    //     if(data.access_token){
+    //         Cookie.set('token', data.access_token, { expires:5 } );
+    //     }
+    // };
 
   return {
     user,
